1f592c358a06be4ad4529aff64b9301622103a73
Alonso Almodóvar <alonsoalmodovar09@gmail.com>
2025-02-27 09:26:33 +0100

    definitive and orderly

--- js/charactersapi.js
+++ js/charactersapi.js
@@ -1,29 +1,29 @@
-function buscarPersonaje(){
+function buscarPersonaje() {
     let nombrePersonaje = document.getElementById("personajeInput").value;
-    let urlApi = `https://thesimpsonsquoteapi.glitch.me/quotes?character=${nombrePersonaje}`
+    let urlApi = `https://thesimpsonsquoteapi.glitch.me/quotes?character=${nombrePersonaje}`;
     fetch(urlApi)
-    .then(response => response.json())
-    .then(datosPersonaje => mostrarPersonaje(datosPersonaje[0]))
-    .catch(()=> mostrarError())
+        .then((response) => response.json())
+        .then((datosPersonaje) => mostrarPersonaje(datosPersonaje[0]))
+        .catch(() => mostrarError());
 }
 
-function mostrarPersonaje(datosPersonaje){
+function mostrarPersonaje(datosPersonaje) {
     let infoDiv = document.getElementById("personajeInfo");
 
     infoDiv.innerHTML = `
     <h2 class="per-name">${datosPersonaje.character.toUpperCase()}</h2>
     <img src="${datosPersonaje.image}" class="per-img">
-    <p><strong>${datosPersonaje.quote}</strong></p>`
+    <p><strong>${datosPersonaje.quote}</strong></p>`;
 }
 
-function mostrarError(){
+function mostrarError() {
     let infoDiv = document.getElementById("personajeInfo");
     infoDiv.innerHTML = `
     <p class="per-err"> <strong>Personaje no encontrado. <br> Intenta con otro nombre </strong></p>
-`
+`;
 }
 
-window.onload = function(){
+window.onload = function () {
     document.getElementById("personajeInput").value = "homer simpson";
     buscarPersonaje();
-} 
+};
